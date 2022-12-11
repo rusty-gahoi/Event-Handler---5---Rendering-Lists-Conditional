24a2fe608b8795d18aaea570eb442d67bf021911
rusty-gahoi <vipulrocks28@gmail.com>
2022-12-11 19:10:00 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [data, setData] = useState("");
+  const handleChange = (event) => {
+    setData(event.target.value)
+
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("form submitted")
+  }
 
 
   return (
     <div id="main">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='name'>Name</label>
-        <input id="name" type={"text"} />
+        <input id="name" type={"text"} value={data} onChange={handleChange} />
 
         <br />
         <br />
@@ -19,3 +28,4 @@ const App = () => {
 
 
 export default App;
+
